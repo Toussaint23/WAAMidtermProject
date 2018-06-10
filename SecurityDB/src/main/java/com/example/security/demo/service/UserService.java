8c94376228fb8a009974f0055df78bd41191c0a0
Toussaint23 <36868582+Toussaint23@users.noreply.github.com>
2018-06-10 11:11:42 -0500

    p

--- SecurityDB/src/main/java/com/example/security/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.security.demo.service;
-
-import com.example.security.demo.model.User;
-
-public interface UserService {
-    public User findUserByEmail(String email);
-    public void saveUser(User user);
-}
